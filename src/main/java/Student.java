3705e27917a4760f8296cb241694c9bec3465f05
Mel <melly.cadd@gmail.com>
2025-02-02 21:18:13 -0700

    Initial Commit

---
+++ src/main/java/Student.java
@@ -0,0 +1,22 @@
+public class Student {
+    private int rollno;
+    private String name;
+    private String address;
+
+    public Student (int rollno, String name, String address) {
+        this.rollno = rollno;
+        this.name = name;
+        this.address = address;
+    }
+
+    // Getters
+    public int getRollno() { return rollno; }
+    public String getName() { return name; }
+    public String getAddress() { return address; }
+
+    @Override
+    public String toString() {
+        return "Student [Roll No: " + rollno + ", Name: " + name + ", Address: " + address + "]";
+    }
+}
+
